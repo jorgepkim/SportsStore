9e4c8f4c310cfa7723dcf3475c1c59b304f87295
skovach <serkovachf@gmail.com>
2014-07-29 15:35:18 +0300

    Initial Commit

---
+++ SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+
+        void SaveProducts(Product product);
+
+        Product DeleteProduct(int productId);
+    }
+}
